b5c4a193f8cfee483101f1468fe06bdaa25c2f17
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-05-27 09:31:42 +0300

    Naming

--- lil_toml/_parser.py
+++ lil_toml/_parser.py
@@ -6,15 +6,15 @@ from typing import Any, Dict, Iterable, Optional, Set, Tuple
 from lil_toml import _re
 from lil_toml._tz import CustomTzinfo
 
-_Namespace = Tuple[str, ...]
+Namespace = Tuple[str, ...]
 
 
-class _ParseState:
+class ParseState:
     def __init__(self, src: str):
         self.src: str = src
         self.pos: int = 0
-        self.out: _NestedDict = _NestedDict({})
-        self.header_namespace: _Namespace = ()
+        self.out: NestedDict = NestedDict({})
+        self.header_namespace: Namespace = ()
 
     def done(self) -> bool:
         return self.pos >= len(self.src)
@@ -23,10 +23,10 @@ class _ParseState:
         return self.src[self.pos]
 
 
-class _NestedDict:
+class NestedDict:
     def __init__(self, wrapped_dict: dict):
         self.dict: Dict[str, Any] = wrapped_dict
-        self.explicitly_defined_keys: Set[Tuple[str, ...]] = set()
+        self.explicitly_created: Set[Tuple[str, ...]] = set()
 
     def __contains__(self, keys: Tuple[str, ...]) -> bool:
         try:
@@ -51,7 +51,7 @@ class _NestedDict:
             container = container[k]
             if isinstance(container, list):
                 container = container[-1]
-        self.explicitly_defined_keys.add(keys)
+        self.explicitly_created.add(keys)
         return container
 
     def append_nest_to_list(self, keys: Tuple[str, ...]) -> dict:
@@ -62,7 +62,7 @@ class _NestedDict:
             container[last_key].append(nest)
         else:
             container[last_key] = [nest]
-        self.explicitly_defined_keys.add(keys)
+        self.explicitly_created.add(keys)
         return nest
 
 
@@ -71,12 +71,12 @@ def loads(s: str) -> dict:  # noqa: C901
     # literals. Let's do so to simplify parsing.
     s = s.replace("\r\n", "\n")
 
-    state = _ParseState(s)
+    state = ParseState(s)
 
     # Parse one statement at a time (typically means one line)
     while True:
         # 0. skip whitespace
-        _skip_chars(state, _TOML_WS)
+        _skip_chars(state, TOML_WS)
 
         # 1. rules
         #      - end of file
@@ -93,7 +93,7 @@ def loads(s: str) -> dict:  # noqa: C901
             continue
         elif char == "#":
             _comment_rule(state)
-        elif char in _BARE_KEY_CHARS or char in "\"'":
+        elif char in BARE_KEY_CHARS or char in "\"'":
             _key_value_rule(state)
         elif state.src[state.pos : state.pos + 2] == "[[":
             _create_list_rule(state)
@@ -103,7 +103,7 @@ def loads(s: str) -> dict:  # noqa: C901
             raise Exception("TODO: msg and type --- not able to apply any rule")
 
         # 2. skip whitespace and line comment
-        _skip_chars(state, _TOML_WS)
+        _skip_chars(state, TOML_WS)
         _skip_comment(state)
 
         # 3. either:
@@ -120,42 +120,42 @@ def loads(s: str) -> dict:  # noqa: C901
     return state.out.dict
 
 
-_TOML_WS = frozenset(" \t")
-_BARE_KEY_CHARS = frozenset(string.ascii_letters + string.digits + "-_")
+TOML_WS = frozenset(" \t")
+BARE_KEY_CHARS = frozenset(string.ascii_letters + string.digits + "-_")
 
 
-def _skip_chars(state: _ParseState, chars: Iterable[str]) -> None:
+def _skip_chars(state: ParseState, chars: Iterable[str]) -> None:
     while not state.done() and state.char() in chars:
         state.pos += 1
 
 
-def _skip_until(state: _ParseState, chars: Iterable[str]) -> None:
+def _skip_until(state: ParseState, chars: Iterable[str]) -> None:
     while not state.done() and state.char() not in chars:
         state.pos += 1
 
 
-def _skip_comment(state: _ParseState) -> None:
+def _skip_comment(state: ParseState) -> None:
     if not state.done() and state.char() == "#":
         _comment_rule(state)
 
 
-def _comment_rule(state: _ParseState) -> None:
+def _comment_rule(state: ParseState) -> None:
     state.pos += 1
     while not state.done():
         c = state.char()
         if c == "\n":
             break
-        if c in _ILLEGAL_COMMENT_CHARS:
+        if c in ILLEGAL_COMMENT_CHARS:
             raise Exception("TODO: msg and type")
         state.pos += 1
 
 
-def _create_dict_rule(state: _ParseState) -> None:
+def _create_dict_rule(state: ParseState) -> None:
     state.pos += 1
-    _skip_chars(state, _TOML_WS)
+    _skip_chars(state, TOML_WS)
     key_parts = _parse_key(state)
 
-    if key_parts in state.out.explicitly_defined_keys:
+    if key_parts in state.out.explicitly_created:
         raise Exception("TODO: msg and type")
     state.out.get_or_create_nest(key_parts)
     state.header_namespace = key_parts
@@ -165,9 +165,9 @@ def _create_dict_rule(state: _ParseState) -> None:
     state.pos += 1
 
 
-def _create_list_rule(state: _ParseState) -> None:
+def _create_list_rule(state: ParseState) -> None:
     state.pos += 2
-    _skip_chars(state, _TOML_WS)
+    _skip_chars(state, TOML_WS)
     key_parts = _parse_key(state)
 
     state.out.append_nest_to_list(key_parts)
@@ -178,53 +178,53 @@ def _create_list_rule(state: _ParseState) -> None:
     state.pos += 2
 
 
-def _key_value_rule(state: _ParseState) -> None:
+def _key_value_rule(state: ParseState) -> None:
     key_parts, value = _parse_key_value_pair(state)
     parent_key, key_stem = key_parts[:-1], key_parts[-1]
 
     # Set the value in the right place in `state.out`
-    nested_dict = _NestedDict(state.out.get_nest(state.header_namespace))
+    nested_dict = NestedDict(state.out.get_nest(state.header_namespace))
     nest = nested_dict.get_or_create_nest(parent_key)
     if key_stem in nest:
         raise Exception("TODO: type and msg")
     nest[key_stem] = value
 
 
-def _parse_key_value_pair(state: _ParseState) -> Tuple[Tuple[str, ...], Any]:
+def _parse_key_value_pair(state: ParseState) -> Tuple[Tuple[str, ...], Any]:
     key_parts = _parse_key(state)
     if state.char() != "=":
         raise Exception("TODO: type and msg")
     state.pos += 1
-    _skip_chars(state, _TOML_WS)
+    _skip_chars(state, TOML_WS)
     value = _parse_value(state)
     return key_parts, value
 
 
-def _parse_key(state: _ParseState) -> Tuple[str, ...]:
+def _parse_key(state: ParseState) -> Tuple[str, ...]:
     """Return parsed key as list of strings.
 
     Move state.pos after the key, to the start of the value that
     follows. Throw if parsing fails.
     """
     key_parts = [_parse_key_part(state)]
-    _skip_chars(state, _TOML_WS)
+    _skip_chars(state, TOML_WS)
     while state.char() == ".":
         state.pos += 1
-        _skip_chars(state, _TOML_WS)
+        _skip_chars(state, TOML_WS)
         key_parts.append(_parse_key_part(state))
-        _skip_chars(state, _TOML_WS)
+        _skip_chars(state, TOML_WS)
     return tuple(key_parts)
 
 
-def _parse_key_part(state: _ParseState) -> str:
+def _parse_key_part(state: ParseState) -> str:
     """Return parsed key part.
 
     Move state.pos after the key part. Throw if parsing fails.
     """
     char = state.char()
-    if char in _BARE_KEY_CHARS:
+    if char in BARE_KEY_CHARS:
         start_pos = state.pos
-        _skip_chars(state, _BARE_KEY_CHARS)
+        _skip_chars(state, BARE_KEY_CHARS)
         return state.src[start_pos : state.pos]
     elif char == "'":
         return _parse_literal_str(state)
@@ -234,17 +234,17 @@ def _parse_key_part(state: _ParseState) -> str:
         raise Exception("TODO: add type and msg")
 
 
-_ASCII_CTRL = frozenset(chr(i) for i in range(32)) | frozenset(chr(127))
+ASCII_CTRL = frozenset(chr(i) for i in range(32)) | frozenset(chr(127))
 
 # Neither of these sets include quotation mark or backslash. They are
 # currently handled as separate cases in the parser functions.
-_ILLEGAL_BASIC_STR_CHARS = _ASCII_CTRL - frozenset("\t")
-_ILLEGAL_MULTILINE_BASIC_STR_CHARS = _ASCII_CTRL - frozenset("\t\n\r")
+ILLEGAL_BASIC_STR_CHARS = ASCII_CTRL - frozenset("\t")
+ILLEGAL_MULTILINE_BASIC_STR_CHARS = ASCII_CTRL - frozenset("\t\n\r")
 
-_ILLEGAL_COMMENT_CHARS = _ASCII_CTRL - frozenset("\t")
+ILLEGAL_COMMENT_CHARS = ASCII_CTRL - frozenset("\t")
 
 
-def _parse_basic_str(state: _ParseState) -> str:
+def _parse_basic_str(state: ParseState) -> str:
     state.pos += 1
     result = ""
     while not state.done():
@@ -252,7 +252,7 @@ def _parse_basic_str(state: _ParseState) -> str:
         if c == '"':
             state.pos += 1
             return result
-        if c in _ILLEGAL_BASIC_STR_CHARS:
+        if c in ILLEGAL_BASIC_STR_CHARS:
             raise Exception("TODO: msg and type")
 
         if c == "\\":
@@ -264,7 +264,7 @@ def _parse_basic_str(state: _ParseState) -> str:
     raise Exception("TODO: msg and type")
 
 
-def _parse_array(state: _ParseState) -> list:
+def _parse_array(state: ParseState) -> list:
     state.pos += 1
     array: list = []
 
@@ -290,8 +290,8 @@ def _parse_array(state: _ParseState) -> list:
             return array
 
 
-def _skip_comments_and_array_ws(state: _ParseState) -> None:
-    array_ws = _TOML_WS | {"\n"}
+def _skip_comments_and_array_ws(state: ParseState) -> None:
+    array_ws = TOML_WS | {"\n"}
     while True:
         pos_before_skip = state.pos
         _skip_chars(state, array_ws)
@@ -300,11 +300,11 @@ def _skip_comments_and_array_ws(state: _ParseState) -> None:
             break
 
 
-def _parse_inline_table(state: _ParseState) -> dict:
+def _parse_inline_table(state: ParseState) -> dict:
     state.pos += 1
-    nested_dict = _NestedDict({})
+    nested_dict = NestedDict({})
 
-    _skip_chars(state, _TOML_WS)
+    _skip_chars(state, TOML_WS)
     if state.char() == "}":
         state.pos += 1
         return nested_dict.dict
@@ -312,17 +312,17 @@ def _parse_inline_table(state: _ParseState) -> dict:
         keys, value = _parse_key_value_pair(state)
         nest = nested_dict.get_or_create_nest(keys[:-1])
         nest[keys[-1]] = value  # TODO: check that "keys[-1]" isnt already there
-        _skip_chars(state, _TOML_WS)
+        _skip_chars(state, TOML_WS)
         if state.char() == "}":
             state.pos += 1
             return nested_dict.dict
         if state.char() != ",":
             raise Exception("TODO: type and msg")
         state.pos += 1
-        _skip_chars(state, _TOML_WS)
+        _skip_chars(state, TOML_WS)
 
 
-_BASIC_STR_ESCAPE_REPLACEMENTS = {
+BASIC_STR_ESCAPE_REPLACEMENTS = {
     "\\b": "\u0008",  # backspace
     "\\t": "\u0009",  # tab
     "\\n": "\u000A",  # linefeed
@@ -333,17 +333,17 @@ _BASIC_STR_ESCAPE_REPLACEMENTS = {
 }
 
 
-def _parse_basic_str_escape_sequence(state: _ParseState, *, multiline: bool) -> str:
+def _parse_basic_str_escape_sequence(state: ParseState, *, multiline: bool) -> str:
     escape_id = state.src[state.pos : state.pos + 2]
     if not len(escape_id) == 2:
         raise Exception("TODO: type and msg")
     state.pos += 2
 
     if multiline and escape_id in {"\\ ", "\\\t", "\\\n"}:
-        _skip_chars(state, _TOML_WS | frozenset("\n"))
+        _skip_chars(state, TOML_WS | frozenset("\n"))
         return ""
-    if escape_id in _BASIC_STR_ESCAPE_REPLACEMENTS:
-        return _BASIC_STR_ESCAPE_REPLACEMENTS[escape_id]
+    if escape_id in BASIC_STR_ESCAPE_REPLACEMENTS:
+        return BASIC_STR_ESCAPE_REPLACEMENTS[escape_id]
     if escape_id == "\\u":
         return _parse_hex_char(state, 4)
     if escape_id == "\\U":
@@ -351,7 +351,7 @@ def _parse_basic_str_escape_sequence(state: _ParseState, *, multiline: bool) -> 
     raise Exception("TODO: type and msg")
 
 
-def _parse_hex_char(state: _ParseState, hex_len: int) -> str:
+def _parse_hex_char(state: ParseState, hex_len: int) -> str:
     hex_str = state.src[state.pos : state.pos + hex_len]
     if not len(hex_str) == hex_len or any(c not in string.hexdigits for c in hex_str):
         raise Exception("TODO: type and msg")
@@ -359,7 +359,7 @@ def _parse_hex_char(state: _ParseState, hex_len: int) -> str:
     return chr(int(hex_str, 16))
 
 
-def _parse_literal_str(state: _ParseState) -> str:
+def _parse_literal_str(state: ParseState) -> str:
     state.pos += 1
     start_pos = state.pos
     _skip_until(state, "'\n")
@@ -370,7 +370,7 @@ def _parse_literal_str(state: _ParseState) -> str:
     return state.src[start_pos:end_pos]
 
 
-def _parse_multiline_literal_str(state: _ParseState) -> str:
+def _parse_multiline_literal_str(state: ParseState) -> str:
     state.pos += 3
     if state.char() == "\n":
         state.pos += 1
@@ -393,7 +393,7 @@ def _parse_multiline_literal_str(state: _ParseState) -> str:
     return state.src[start_pos:end_pos]
 
 
-def _parse_multiline_basic_str(state: _ParseState) -> str:
+def _parse_multiline_basic_str(state: ParseState) -> str:
     state.pos += 3
     if state.char() == "\n":
         state.pos += 1
@@ -420,7 +420,7 @@ def _parse_multiline_basic_str(state: _ParseState) -> str:
                 result += '"'
                 state.pos += 1
             continue
-        if c in _ILLEGAL_MULTILINE_BASIC_STR_CHARS:
+        if c in ILLEGAL_MULTILINE_BASIC_STR_CHARS:
             raise Exception("TODO: msg and type")
 
         if c == "\\":
@@ -432,7 +432,7 @@ def _parse_multiline_basic_str(state: _ParseState) -> str:
     raise Exception("TODO: msg and type")
 
 
-def _parse_regex(state: _ParseState, regex: re.Pattern) -> str:
+def _parse_regex(state: ParseState, regex: re.Pattern) -> str:
     match = regex.match(state.src[state.pos :])
     if not match:
         raise Exception("TODO: type and msg")
@@ -441,7 +441,7 @@ def _parse_regex(state: _ParseState, regex: re.Pattern) -> str:
     return match_str
 
 
-def _parse_value(state: _ParseState) -> Any:  # noqa: C901
+def _parse_value(state: ParseState) -> Any:  # noqa: C901
     src = state.src[state.pos :]
     char = state.char()
 
