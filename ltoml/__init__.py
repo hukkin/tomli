14182cd4f2b12c3544bb6a647c07eec5041db297
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-05-27 19:32:05 +0300

    Bump version: 0.0.0 → 0.1.0

--- ltoml/__init__.py
+++ ltoml/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ("loads", "TOMLDecodeError")
-__version__ = "0.0.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "0.1.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 from ltoml._parser import TOMLDecodeError, loads
