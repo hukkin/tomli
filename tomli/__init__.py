6c3e23870df1eb02584711aa82aee81d90621c26
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-05-29 02:01:42 +0300

    Bump version: 0.2.1 → 0.2.2

--- tomli/__init__.py
+++ tomli/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ("loads", "load", "TOMLDecodeError")
-__version__ = "0.2.1"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "0.2.2"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 from tomli._parser import TOMLDecodeError, load, loads
