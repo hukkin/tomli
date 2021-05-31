6c671287b23d851bad1f4d441a7676412bb1d5d8
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-05-31 22:49:07 +0300

    Tox env for profiler

--- tests/test_for_profiler.py
+++ tests/test_for_profiler.py
@@ -2,11 +2,11 @@
 
 This test can be useful for profiling, as most of the execution time
 will be spent parsing and rendering instead of managing pytest execution
-environment. To get profiler results:
-  - `pip install pytest-profiling`
-  - `pytest -k test_for_profiler --profile-svg`
-  - `firefox prof/combined.svg`
+environment. To get and read profiler results:
+  - `tox -e profile`
+  - `firefox .tox/prof/combined.svg`
 """
+import os
 from pathlib import Path
 
 import tomli
@@ -17,7 +17,8 @@ def test_for_profiler():
     benchmark_toml = path.read_text("utf-8")
     # increase the count here to reduce the impact of
     # setting up pytest execution environment. Let's keep
-    # the count low here because this is part of the
+    # the count low by default because this is part of the
     # standard test suite.
-    for _ in range(10):
+    iterations = int(os.environ.get("PROFILER_ITERATIONS", 1))
+    for _ in range(iterations):
         tomli.loads(benchmark_toml)
