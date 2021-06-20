e98b8c4398d62ef7178147d2e6312e9a0d869b79
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2021-06-20 15:22:48 +0300

    Consistent regex naming

--- tomli/_re.py
+++ tomli/_re.py
@@ -15,12 +15,12 @@ _TIME_RE_STR = r"([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?"
 RE_HEX = re.compile(r"[0-9A-Fa-f](?:_?[0-9A-Fa-f])*")
 RE_BIN = re.compile(r"[01](?:_?[01])*")
 RE_OCT = re.compile(r"[0-7](?:_?[0-7])*")
-RE_DEC_OR_FLOAT = re.compile(
+RE_NUMBER = re.compile(
     r"[+-]?(?:0|[1-9](?:_?[0-9])*)"  # integer
     + r"(?:\.[0-9](?:_?[0-9])*)?"  # optional fractional part
     + r"(?:[eE][+-]?[0-9](?:_?[0-9])*)?"  # optional exponent part
 )
-RE_LOCAL_TIME = re.compile(_TIME_RE_STR)
+RE_LOCALTIME = re.compile(_TIME_RE_STR)
 RE_DATETIME = re.compile(
     r"([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[01])"  # date, e.g. 1988-10-27
     + r"(?:"
