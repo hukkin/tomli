34ef1f1b97f27514b810bbb09809d870d0bb02b4
Thomas Grainger <tagrain@gmail.com>
2021-07-09 17:58:27 +0100

    add a handy alias for TOMLDecodeError

--- tomli/__init__.py
+++ tomli/__init__.py
@@ -4,3 +4,4 @@ __all__ = ("loads", "load", "TOMLDecodeError")
 __version__ = "1.0.4"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 from tomli._parser import TOMLDecodeError, load, loads
+from tomli._parser import TOMLDecodeError as TomlDecodeError
