d71040aa65f95038f7b70b18ebe398c358d58add
Michael Wayne Goodman <goodman.m.w@gmail.com>
2021-10-22 15:56:55 -0700

    Move __module__ assignment to __init__, add test

--- tomli/__init__.py
+++ tomli/__init__.py
@@ -4,3 +4,6 @@ __all__ = ("loads", "load", "TOMLDecodeError")
 __version__ = "1.2.1"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 from tomli._parser import TOMLDecodeError, load, loads
+
+# Pretend this exception was created here.
+TOMLDecodeError.__module__ = 'tomli'
--- tomli/_parser.py
+++ tomli/_parser.py
@@ -61,8 +61,6 @@ Pos = int
 class TOMLDecodeError(ValueError):
     """An error raised if a document is not valid TOML."""
 
-    __module__ = "tomli"
-
 
 def load(fp: BinaryIO, *, parse_float: ParseFloat = float) -> Dict[str, Any]:
     """Parse TOML from a binary file object."""
