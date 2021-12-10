2035b199e344e46fdc3c84df5b96bdddb63378a3
Oittaa <eero@oittaa.net>
2021-12-10 09:18:29 +0100

    Remove numpy

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,7 +1,5 @@
 from datetime import datetime, time
 
-import numpy as np
-
 import tomli
 
 
@@ -9,6 +7,12 @@ def custom_parse_time(time_str: str) -> time:
     """Strips the fractional part from seconds."""
     time_str = time_str.split(".")[0]
     return time.fromisoformat(time_str)
+
+
+def custom_parse_datetime(time_str: str) -> datetime:
+    """Strips the fractional part from seconds."""
+    time_str = time_str.split(".")[0]
+    return datetime.fromisoformat(time_str)
 
 
 def test_datetime_w_nanoseconds():
@@ -34,19 +38,6 @@ def test_datetime_w_nanoseconds():
     assert obj == expected_obj
 
 
-def test_numpy_w_nanoseconds():
-    """Nanosecond precision in datetime64 objects."""
-    doc = """
-          [bliibaa]
-          datetime=1979-05-27T00:32:00.999999999
-          """
-    obj = tomli.loads(doc, parse_datetime=np.datetime64)
-    expected_obj = {
-        "bliibaa": {"datetime": np.datetime64("1979-05-27T00:32:00.999999999")}
-    }
-    assert obj == expected_obj
-
-
 def test_custom_parse_time():
     """Test parser that strips the fractional part."""
     doc = """
@@ -55,4 +46,17 @@ def test_custom_parse_time():
           """
     obj = tomli.loads(doc, parse_time=custom_parse_time)
     expected_obj = {"bliibaa": {"time": time.fromisoformat("00:32:00")}}
+    assert obj == expected_obj
+
+
+def test_custom_parse_datetime():
+    """Test parser that strips the fractional part."""
+    doc = """
+          [bliibaa]
+          datetime=1979-05-27T00:32:00.999999999
+          """
+    obj = tomli.loads(doc, parse_datetime=custom_parse_datetime)
+    expected_obj = {
+        "bliibaa": {"datetime": datetime.fromisoformat("1979-05-27T00:32:00")}
+    }
     assert obj == expected_obj
