afac59130ed0da2d69a1c2f50ca8f70663a5bd88
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2022-01-03 18:39:21 +0200

    Migrate test_misc.py to unittest

--- tests/test_error.py
+++ tests/test_error.py
@@ -7,29 +7,33 @@ class TestError(unittest.TestCase):
     def test_line_and_col(self):
         with self.assertRaises(tomli.TOMLDecodeError) as exc_info:
             tomli.loads("val=.")
-        assert str(exc_info.exception) == "Invalid value (at line 1, column 5)"
+        self.assertEqual(str(exc_info.exception), "Invalid value (at line 1, column 5)")
 
         with self.assertRaises(tomli.TOMLDecodeError) as exc_info:
             tomli.loads(".")
-        assert str(exc_info.exception) == "Invalid statement (at line 1, column 1)"
+        self.assertEqual(
+            str(exc_info.exception), "Invalid statement (at line 1, column 1)"
+        )
 
         with self.assertRaises(tomli.TOMLDecodeError) as exc_info:
             tomli.loads("\n\nval=.")
-        assert str(exc_info.exception) == "Invalid value (at line 3, column 5)"
+        self.assertEqual(str(exc_info.exception), "Invalid value (at line 3, column 5)")
 
         with self.assertRaises(tomli.TOMLDecodeError) as exc_info:
             tomli.loads("\n\n.")
-        assert str(exc_info.exception) == "Invalid statement (at line 3, column 1)"
+        self.assertEqual(
+            str(exc_info.exception), "Invalid statement (at line 3, column 1)"
+        )
 
     def test_missing_value(self):
         with self.assertRaises(tomli.TOMLDecodeError) as exc_info:
             tomli.loads("\n\nfwfw=")
-        assert str(exc_info.exception) == "Invalid value (at end of document)"
+        self.assertEqual(str(exc_info.exception), "Invalid value (at end of document)")
 
     def test_invalid_char_quotes(self):
         with self.assertRaises(tomli.TOMLDecodeError) as exc_info:
             tomli.loads("v = '\n'")
-        assert " '\\n' " in str(exc_info.exception)
+        self.assertTrue(" '\\n' " in str(exc_info.exception))
 
     def test_module_name(self):
-        assert tomli.TOMLDecodeError().__module__ == "tomli"
+        self.assertEqual(tomli.TOMLDecodeError().__module__, "tomli")
