07501599634283c952861db08815e6710b440721
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2022-01-03 17:39:56 +0200

    Migrate test_error.py to unittest

--- tests/test_error.py
+++ tests/test_error.py
@@ -1,37 +1,35 @@
-import pytest
+import unittest
 
 import tomli
 
 
-def test_line_and_col():
-    with pytest.raises(tomli.TOMLDecodeError) as exc_info:
-        tomli.loads("val=.")
-    assert str(exc_info.value) == "Invalid value (at line 1, column 5)"
+class TestError(unittest.TestCase):
+    def test_line_and_col(self):
+        with self.assertRaises(tomli.TOMLDecodeError) as exc_info:
+            tomli.loads("val=.")
+        assert str(exc_info.exception) == "Invalid value (at line 1, column 5)"
 
-    with pytest.raises(tomli.TOMLDecodeError) as exc_info:
-        tomli.loads(".")
-    assert str(exc_info.value) == "Invalid statement (at line 1, column 1)"
+        with self.assertRaises(tomli.TOMLDecodeError) as exc_info:
+            tomli.loads(".")
+        assert str(exc_info.exception) == "Invalid statement (at line 1, column 1)"
 
-    with pytest.raises(tomli.TOMLDecodeError) as exc_info:
-        tomli.loads("\n\nval=.")
-    assert str(exc_info.value) == "Invalid value (at line 3, column 5)"
+        with self.assertRaises(tomli.TOMLDecodeError) as exc_info:
+            tomli.loads("\n\nval=.")
+        assert str(exc_info.exception) == "Invalid value (at line 3, column 5)"
 
-    with pytest.raises(tomli.TOMLDecodeError) as exc_info:
-        tomli.loads("\n\n.")
-    assert str(exc_info.value) == "Invalid statement (at line 3, column 1)"
+        with self.assertRaises(tomli.TOMLDecodeError) as exc_info:
+            tomli.loads("\n\n.")
+        assert str(exc_info.exception) == "Invalid statement (at line 3, column 1)"
 
+    def test_missing_value(self):
+        with self.assertRaises(tomli.TOMLDecodeError) as exc_info:
+            tomli.loads("\n\nfwfw=")
+        assert str(exc_info.exception) == "Invalid value (at end of document)"
 
-def test_missing_value():
-    with pytest.raises(tomli.TOMLDecodeError) as exc_info:
-        tomli.loads("\n\nfwfw=")
-    assert str(exc_info.value) == "Invalid value (at end of document)"
+    def test_invalid_char_quotes(self):
+        with self.assertRaises(tomli.TOMLDecodeError) as exc_info:
+            tomli.loads("v = '\n'")
+        assert " '\\n' " in str(exc_info.exception)
 
-
-def test_invalid_char_quotes():
-    with pytest.raises(tomli.TOMLDecodeError) as exc_info:
-        tomli.loads("v = '\n'")
-    assert " '\\n' " in str(exc_info.value)
-
-
-def test_module_name():
-    assert tomli.TOMLDecodeError().__module__ == "tomli"
+    def test_module_name(self):
+        assert tomli.TOMLDecodeError().__module__ == "tomli"
