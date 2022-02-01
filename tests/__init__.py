ecc36aa75143cd09eed0368d540857492179b667
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2022-02-02 00:41:34 +0200

    Improve/add license headers (#176)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2021 Taneli Hukkinen
+# Licensed to PSF under a Contributor Agreement.
+
 __all__ = ("tomllib",)
 
 # By changing this one line, we can run the tests against
