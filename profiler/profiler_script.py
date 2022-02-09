ad96211be16d7365505a54f8c227d62729d7769c
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2022-02-09 02:12:57 +0200

    Update fuzzer, profiler, and benchmark results

---
+++ profiler/profiler_script.py
@@ -0,0 +1,17 @@
+"""A script for profiling.
+
+To generate and read results:
+  - `tox -e profile`
+  - `firefox .tox/prof/output.svg`
+"""
+from pathlib import Path
+
+import tomli
+
+benchmark_toml = (
+    (Path(__file__).parent.parent / "benchmark" / "data.toml").read_bytes().decode()
+)
+
+# Run this a few times to emphasize over imports and other overhead above.
+for _ in range(1000):
+    tomli.loads(benchmark_toml)
